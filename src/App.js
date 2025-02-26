06959b861c5f049d05faeedad910406fb49395f7
Prathamesh Yelpale <prathmesh735@gmail.com>
2025-02-26 11:26:42 +0530

    my account page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import UpdateClientStatus from "./Components/UpdateClientStatus";
 import Login from "./Components/Login";
 import AddPackages from './Components/AddPackages';
 import Homepage from "./Components/Homepage";
+import MyAccountDashboard from "./Components/MyAccount";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Homepage} />
           <Route exact path="/dashboard" Component={Dashboard} />
+          <Route exact path="/myAccount" Component={MyAccountDashboard} />
           <Route exact path="/login" Component={Login} />
           <Route exact path="/addClient" Component={AddClient} />
           <Route exact path="/updateClientStatus/:id" Component={UpdateClientStatus} />
